053c94d1acc179412932aaeb1bbbe4f7da8b62a2
rodriguezUsmaAf <58751947+rodriguezUsmaAf@users.noreply.github.com>
2019-12-11 08:47:11 -0500

    Update TestGit1.java

--- testGit1/src/testgit1/TestGit1.java
+++ testGit1/src/testgit1/TestGit1.java
@@ -17,6 +17,7 @@ public class TestGit1 {
     public static void main(String[] args) {
         System.out.println("HOLA");
         System.out.println("Flame Sniper!");
+        System.out.println("Fire soul!");
     }
     
 }
