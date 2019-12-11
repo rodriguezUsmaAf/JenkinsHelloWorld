34cbbd2e09b317e973359e110031b97f5721ccd8
afrodriguezu <afrodriguezu@afrodriguezu7.indra.es>
2019-12-11 08:48:51 -0500

    Corrección soul por Soul

--- testGit1/src/testgit1/TestGit1.java
+++ testGit1/src/testgit1/TestGit1.java
@@ -17,7 +17,7 @@ public class TestGit1 {
     public static void main(String[] args) {
         System.out.println("HOLA");
         System.out.println("Flame Sniper!");
-        System.out.println("Fire soul!");
+        System.out.println("Fire Soul!");
     }
     
 }
